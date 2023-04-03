da993fe166796e8167b9bf6257d73ae80895fc79
Tommaso Seresi <tommaso.seresi@stdenti.unicam.it>
2023-04-03 23:28:09 +0200

    All Configuration & add Register pages

---
+++ public/chat-app/src/pages/Register.jsx
@@ -0,0 +1,114 @@
+import React, {useState, useEffect} from 'react';
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Logo from "../assets/LogoAPP.PNG"
+import {ToastContainer, toast} from "react-toastify";
+function Register() {
+    const [values, setValues] = useState({
+        uusername:"",
+        email:"",
+        password:"",
+        confirmPassword: "",
+    });
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+        alert("form");
+    };
+
+    const handleValidation = ()=>{
+        const {password, confirmPassword, uusername, email} = values;
+        if(password!==confirmPassword){
+            toast.error("password and confirm password should be same.");
+        }
+    };
+
+    const handleChange = (event)=>{
+        setValues({...values,[event.target.name]: event.target.value });
+    };
+    return (
+        <>
+        <FormContainer>
+            <form onSubmit={(event)=>handleSubmit(event)}>
+                <div className="brand">
+                    <img src={Logo} alt="Logo"/>
+                </div>
+                <input type="text" placeholder="Username" name="username" onChange={(e) => handleChange(e)}/>
+                <input type="email" placeholder="Email" name="email" onChange={(e) => handleChange(e)}/>
+                <input type="password" placeholder="Password" name="password" onChange={(e) => handleChange(e)}/>
+                <input type="password" placeholder="Confirm Password" name="confirmPassword" onChange={(e) => handleChange(e)}/>
+                <button type="submit">Create User</button>
+                <span>
+                    already have an account ?<Link to="/login">Login.</Link>
+                </span>
+            </form>
+        </FormContainer>
+        <ToastContainer />
+        </>
+    );
+}
+
+const FormContainer = styled.div`
+height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 7rem;
+    }
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000076;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+  }
+  input {
+    background-color: transparent;
+    padding: 1rem;
+    border: 0.1rem solid #4e0eff;
+    border-radius: 0.4rem;
+    color: white;
+    width: 100%;
+    font-size: 1rem;
+    &:focus {
+      border: 0.1rem solid #997af0;
+      outline: none;
+    }
+  }
+  button {
+    background-color: #4e0eff;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      background-color: #4e0eff;
+    }
+  }
+  span {
+    color: white;
+    text-transform: uppercase;
+    a {
+      color: #4e0eff;
+      text-decoration: none;
+      font-weight: bold;
+    }
+  }
+`;
+
+export default Register;
